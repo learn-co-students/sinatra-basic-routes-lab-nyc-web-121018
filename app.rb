d1a50bba0aab5f1cea66c24a5498daa9166eb40b
sharicodes <srosenb@yahoo.com>
2019-01-05 17:34:10 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Shari"
+  #@status = 200
+end
+get '/hometown' do
+  "My hometown is NYC"
+  #@status = 200
+end
+get '/favorite-song' do
+  "My favorite song is 'Thank U next'"
+#  @status = 200
+end
+
+
 end
