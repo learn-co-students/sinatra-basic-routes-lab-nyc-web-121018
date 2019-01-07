2afa83f2336acb77d3d427392cb5b84c35c723a7
pirainogi <pirainogi@gmail.com>
2019-01-06 21:38:31 -0500

    finished

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Gabbie"
+  end
+
+  get '/hometown' do
+    "My hometown is Lancaster, PA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is thank u, next"
+  end 
+
 end
